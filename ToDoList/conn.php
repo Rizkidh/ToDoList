d005a2d8cf1d68f351e24207502bfff37b71957c
Rizkidh <106050250+Rizkidh@users.noreply.github.com>
2023-10-15 11:26:32 +0700

    Add files via upload

---
+++ ToDoList/conn.php
@@ -0,0 +1,8 @@
+<?php 
+$databaseHost = 'localhost';
+$databaseName = 'todolist';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, 
+    $databasePassword, $databaseName);
